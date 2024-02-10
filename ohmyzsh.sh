c88ebde64dbff7774b57772e7495f583b0f778fb
Jay Shin <okayusimp@proton.me>
2024-02-11 02:24:37 +0900

    zsh installation script

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Install Oh My Zsh
-
 touch ~/.zshrc
 echo "export ZSH="$HOME/.oh-my-zsh"" >> ~/.zshrc
 echo "ZSH_THEME="powlerlevel10k/powerlevel10k"" >> ~/.zshrc
@@ -16,5 +13,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/
 # plugins
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install Oh My Zsh
 
 exec zsh
