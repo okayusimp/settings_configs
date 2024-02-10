5f9aa487e7cf1f5e7e47cf9eb479803d6cfcafc4
Jay Shin <okayusimp@proton.me>
2024-02-11 02:56:20 +0900

    changes

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -3,9 +3,10 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install Oh My Zsh
 
+rm ~/.zshrc
 touch ~/.zshrc
 echo "export ZSH="$HOME/.oh-my-zsh"" >> ~/.zshrc
-echo "ZSH_THEME="powlerlevel10k/powerlevel10k"" >> ~/.zshrc
+echo "ZSH_THEME="powerlevel10k/powerlevel10k"" >> ~/.zshrc
 echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 echo "source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
 # Edit .zshrc
@@ -13,8 +14,8 @@ echo "source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Powerlevel10k
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightning
 # plugins
 
 exec zsh
